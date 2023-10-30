054cc86093d4f4136f910c361ec789441c0fbb0d
Dipak1121 <91940812+Dipak1121@users.noreply.github.com>
2023-10-30 17:37:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-function volume_sphere() {
+let form = document.getElementById("MyForm");
+form.addEventListener("submit", volume_sphere);
+function volume_sphere(event) {
     //Write your code here
-  
+	event.preventDefault();
+	let radius = form.radius.value;
+	let volume = 4*(Math.PI)*(radius**3)/3;
+	form.volume.value = volume;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
